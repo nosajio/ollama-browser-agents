a77effd0f3f609162727e2c3e368440a7bd5083c
Jason Howmans <jason@nosaj.io>
2023-12-30 01:17:05 +0000

    Fix build

--- src/manifest.ts
+++ src/manifest.ts
@@ -13,7 +13,6 @@ export default defineManifest({
     128: 'img/logo-128.png',
   },
   action: {
-    default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
   options_page: 'options.html',
